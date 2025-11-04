ddbced285a49dd69694fccd850e56c2550286a58
renatobss <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>
2025-11-03 21:02:25 -0300

    criação da função tabuada

---
+++ FuncaoTabuada.js
@@ -0,0 +1,7 @@
+function tabuada(numTabuada){
+    for (let i = 1; i <= 10; i++)
+        console.log(i, "x", numTabuada, "= ", i * numTabuada);
+}
+
+console.log("Retorno da tabuada:");
+tabuada(10);
