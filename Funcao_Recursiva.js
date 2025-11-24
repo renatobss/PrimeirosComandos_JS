d99cdf8770b1d3d86425a4bc2a4c3e89587130f1
renatobss <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>
2025-11-23 22:40:41 -0300

    resolvendo exercicios 23/11

---
+++ Funcao_Recursiva.js
@@ -0,0 +1,9 @@
+function recursiva(inicio, fim) {
+    for (let i = inicio; i <= fim; i++) {
+        console.log(i)        
+    }
+}
+recursiva(12, 20)
+
+
+//Crie uma função recursiva que recebe um número inicial e um limite, e imprime todos os valores até chegar ao limite.
--- aaaaaaaa.js
+++ aaaaaaaa.js
@@ -1,6 +1,21 @@
-let numero = 5
-let p
+const numeros = [1, 2, 2, 3, 3, 4]
 
-p = numero % 2 == 0
+console.log(numeros)
 
-console.log(p)
+let verifica = 0
+let saida = []
+
+for (let i = 0; i < numeros.length; i++) {
+    //console.log(numeros[i])
+    if (numeros[i] != verifica) {
+        verifica = numeros[i]
+        saida.push(numeros[i])
+    }
+}
+
+console.log(saida)
+
+
+//Função que remove itens repetidos de um array
+//Entrada: [1, 2, 2, 3, 3, 4]
+//Saída esperada: [1, 2, 3, 4]
